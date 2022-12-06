a9f7fd56a503ac712eaf57a96b9b8eae55736cc4
wlgns410 <wlgns410@gmail.com>
2022-12-06 18:05:48 +0900

    add : api call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import './App.css'
 import DiaryEditor from './DiaryEditor'
 import DiaryList from './DiaryList'
-import Lifecycle from './Lifecycle'
+
 
 // const dummyList = [
 //   {
@@ -30,7 +30,27 @@ import Lifecycle from './Lifecycle'
 
 function App() {
   const [data, setData] = useState([])
-  const dateId = useRef(0)
+  const dataId = useRef(0)
+  const getData = async() =>{
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res) => res.json());
+    console.log(res);
+
+    const initData = res.slice(0,20).map((it)=>{
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() *5)+1,
+        created_date : new Date().getTime(),
+        id: dataId.current++
+      }
+    });
+    setData(initData);
+  };
+
+  useEffect(()=>{
+    getData();
+  },[])
+
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime()
     const newItem = {
@@ -38,9 +58,9 @@ function App() {
       content,
       emotion,
       created_date,
-      id: dateId.current,
+      id: dataId.current,
     }
-    dateId.current += 1
+    dataId.current += 1
     setData([newItem, ...data])
   }
 
@@ -58,7 +78,6 @@ function App() {
   }
   return (
     <div className="App">
-      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onDelete={onRemove} diaryList={data} />
     </div>
